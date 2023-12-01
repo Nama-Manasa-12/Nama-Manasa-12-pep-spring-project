24a9a86c88f06aa024d968da2013ec84cc7c8a63
Nama-Manasa-12 <Namamanasa@12>
2023-12-01 15:01:20 +0530

    Pep spring project completed

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,20 +1,12 @@
+
+
 package com.example.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.entity.Account;
-public interface AccountRepository extends JpaRepository<Account, Long> {
+
+public interface AccountRepository  extends JpaRepository<Account, Integer>{
+
     Account findByUsername(String username);
 }
-// public interface accountRepository extends JpaRepository<Account,Long>{
-//     Optional<Account> registerUser()
-// }
-//login
-
-// @Repository
-// interface AccountDAO extends JpaRepository<Account, Long> {
-//     Account getAccountByUsername(Account account);
-// }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,58 +1,23 @@
 package com.example.service;
 
-import java.sql.SQLException;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.entity.Account;
-import com.example.repository.AccountRepository;
-@Service
+
+import java.util.List;
+
 public interface AccountService {
 
+    List<Account> getAllAccounts();
+
+    Account getAccountById(Integer id);
 
     Account saveAccount(Account account);
 
+    Account updateAccount(Integer id, Account account);
+
+    void deleteAccount(Integer id);
+
     Account getAccountByUsername(String username);
 
+    Account authenticate(String username, String password);
 
-    //Account findByUsername(String username);
-    
-    // private AccountRepository accountRepository;
-
-    // public Optional<Account> findByUsername(String username) {
-    //     return accountRepository.findByUsername(username);
-    // }
-
-    // // public Account registerUser(Account account) {
-    // //     Account savedAccount = accountRepository.save(account);
-    // //     //return savedAccount;
-    // // }
-
-    //Account saveAccount(Account account);
-//login
-
-
-    // @Autowired
-    // private AccountService accountService;
-
-    // public Account loginUser(Account account) throws SQLException {
-    //     Account loggedInAccount = accountService.getAccountByUsername(account);
-
-    //     if (account != null && loggedInAccount != null && account.getUsername() != null
-    //             && loggedInAccount.getUsername() != null && account.getPassword() != null
-    //             && loggedInAccount.getPassword() != null && account.getUsername().equals(loggedInAccount.getUsername())
-    //             && account.getPassword().equals(loggedInAccount.getPassword())) {
-    //         return loggedInAccount;
-    //     } else {
-    //         return null;
-    //     }
-    // }
-
-    // private Account getAccountByUsername(Account account) {
-    //     return null;
-    // }
-
-        //Account  getAccountByUsername(String username);
 }
